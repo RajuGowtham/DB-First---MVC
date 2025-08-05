3ccb7b6d6df9407a8344eaa36ecd47865cfa81f1
Raju Gowtham <165984076+RajuGowtham@users.noreply.github.com>
2025-08-05 19:05:01 +0530

    Add files via upload

---
+++ Controllers/WorkersController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CodeFirstApplication.Models;
+
+namespace CodeFirstApplication.Controllers
+{
+    public class WorkersController : Controller
+    {
+        private readonly WorkersContext _context;
+
+        public WorkersController(WorkersContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Workers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Workers.ToListAsync());
+        }
+
+        // GET: Workers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workers = await _context.Workers
+                .FirstOrDefaultAsync(m => m.workerId == id);
+            if (workers == null)
+            {
+                return NotFound();
+            }
+
+            return View(workers);
+        }
+
+        // GET: Workers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Workers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("workerId,workerName,Role,DailyCost")] Workers workers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(workers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(workers);
+        }
+
+        // GET: Workers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workers = await _context.Workers.FindAsync(id);
+            if (workers == null)
+            {
+                return NotFound();
+            }
+            return View(workers);
+        }
+
+        // POST: Workers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("workerId,workerName,Role,DailyCost")] Workers workers)
+        {
+            if (id != workers.workerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(workers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkersExists(workers.workerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(workers);
+        }
+
+        // GET: Workers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workers = await _context.Workers
+                .FirstOrDefaultAsync(m => m.workerId == id);
+            if (workers == null)
+            {
+                return NotFound();
+            }
+
+            return View(workers);
+        }
+
+        // POST: Workers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var workers = await _context.Workers.FindAsync(id);
+            if (workers != null)
+            {
+                _context.Workers.Remove(workers);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WorkersExists(int id)
+        {
+            return _context.Workers.Any(e => e.workerId == id);
+        }
+    }
+}
