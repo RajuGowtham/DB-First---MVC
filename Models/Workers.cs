51caa228c52bfb42f1162342591ee355b5bd576c
Raju Gowtham <165984076+RajuGowtham@users.noreply.github.com>
2025-08-05 19:06:00 +0530

    Add files via upload

---
+++ Models/Workers.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstApplication.Models
+{
+    public class Workers
+    {
+            [Key]
+            public int workerId { get; set; }
+            public string workerName { get; set; }
+            public string Role { get; set; }
+            public decimal DailyCost { get; set; }
+    }
+}
---
+++ Models/WorkersContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApplication.Models
+{
+    public class WorkersContext : DbContext
+    {
+        public DbSet<Workers> Workers { get; set; }
+
+        public WorkersContext(DbContextOptions<WorkersContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=RAJU_GOWTHAM;database=practice;integrated security=true;trustservercertificate=true;");
+        }
+    }
+}
